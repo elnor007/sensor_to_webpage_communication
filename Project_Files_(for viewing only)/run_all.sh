e35a18799d534ca603a9980a85563f71ff754b84
elijus norkevicius <elnor888@gmail.com>
2025-09-21 18:25:12 +0100

    Update run_all.sh

--- Project_Files_(for viewing only)/run_all.sh
+++ Project_Files_(for viewing only)/run_all.sh
@@ -9,7 +9,7 @@ cleanup() {
 # Trap Ctrl+C (SIGINT) and termination (SIGTERM)
 trap cleanup SIGINT SIGTERM
 
-
+# Runs the docker compose file and only continues if all containers are up
 docker-compose up -d
 
 echo -e "\n\n$(date +%Y-%m-%d__%H:%M:%S) : Waiting for containers to start...\n"
@@ -20,8 +20,8 @@ done
 echo -e "$(date +%Y-%m-%d__%H:%M:%S) : All containers are running...\n\n"
 
 
-/usr/bin/python3 sensor_sim.py &
+/usr/bin/python3 sensor_sim.py &  # By default, the mock sensor data script runs for convenient testing. If you already have a sensor connected, then you can remove this line
 
-/usr/bin/python3 Stream_endpoint.py &
+/usr/bin/python3 Stream_endpoint.py &  # This polls the database and streams to the frontend
 
-/usr/bin/python3 -m http.server 5500
+/usr/bin/python3 -m http.server 5500  
